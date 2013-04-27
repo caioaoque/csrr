e64720564b638ad17e110c0f57b457be4c926f2e
caioaoque <caioaoque@gmail.com>
2013-04-27 18:36:26 -0300

    Removendo acentuação para o build do travis funcionar.
    Haviam faltado alguns acentos.

--- src/main/java/sistemasExternos/interfaces/Banco.java
+++ src/main/java/sistemasExternos/interfaces/Banco.java
@@ -6,9 +6,9 @@ public interface Banco {
 
 	boolean aprovarSaque(Double Valor, String Conta);
 
-	boolean aprovarDepósito(Double Valor, String Conta); // TODO como avisar que o valor ja foi depositado? criar um novo metodo?	
+	boolean aprovarDeposito(Double Valor, String Conta); // TODO como avisar que o valor ja foi depositado? criar um novo metodo?	
 
-	boolean autorizarTransferência(String ContaDebitada, String ContaCreditada);
+	boolean autorizarTransferencia(String ContaDebitada, String ContaCreditada);
 
 	String obterSaldo(String Conta);	
 	
