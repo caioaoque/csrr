bdf1631491fc49afefe8d2183c4afd4de10d5f7f
samueltmartinez <samuel.t.martinez@gmail.com>
2013-05-13 20:18:59 -0300

    Correção da interface do sistema externo "Banco"

--- src/main/java/br/mackenzie/caixaEletronico/sistemasExternos/interfaces/Banco.java
+++ src/main/java/br/mackenzie/caixaEletronico/sistemasExternos/interfaces/Banco.java
@@ -1,9 +1,18 @@
 package br.mackenzie.caixaEletronico.sistemasExternos.interfaces;
 
+import br.mackenzie.caixaEletronico.sistemaPrincipal.Sessao;
+
 public interface Banco {
-    public abstract String iniciarSessao(String conta, String senha);
+    public abstract Sessao iniciarSessao(String conta, String senha);
+    
     public abstract boolean aprovarSaque(String sessao, double valor);
-    public abstract String getUltimoErro();
-    public abstract void setUltimoErro(String lastError);
+    
+    public void realizarTransferencia();
+    
+	boolean aprovarDeposito(Double Valor, String Conta); 	
+
+	boolean autorizarTransferencia(String ContaDebitada, String ContaCreditada);
+
+	Double obterSaldo(String Conta);    
     
 }
