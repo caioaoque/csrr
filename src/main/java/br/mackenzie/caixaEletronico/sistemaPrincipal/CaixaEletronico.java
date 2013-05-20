128ba981621642649a3f1f253b7f5b5ed10ed119
caioaoque <caioaoque@gmail.com>
2013-05-20 20:49:46 -0300

    Ajustes no saque, criado estado do caixa eletrônico.

--- src/main/java/br/mackenzie/caixaEletronico/sistemaPrincipal/CaixaEletronico.java
+++ src/main/java/br/mackenzie/caixaEletronico/sistemaPrincipal/CaixaEletronico.java
@@ -5,12 +5,20 @@ import br.mackenzie.caixaEletronico.sistemasExternos.interfaces.Console;
 
 public class CaixaEletronico {
 
+	private static enum Estado { OPERANTE, INOPERANTE; }
+
 	private final Banco banco;
 	private final Console console;
+	private Estado estado;
+	private double valorDisponivel;
 
 	public CaixaEletronico(Banco banco, Console console) {
 		this.banco = banco;
 		this.console = console;
+	}
+
+	public boolean iniciarSessao(String cartao, String senha, String conta) {
+		return false;
 	}
 
 	public boolean sacarValor(double valor, String conta, Sessao sessao) {
@@ -18,6 +26,8 @@ public class CaixaEletronico {
 			console.imprimir("Valor inválido. Entre com um valor maior que zero.");
 		} else if (valor % 10 != 0) {
 			console.imprimir("Valor inválido. Entre com um valor múltiplo de dez.");
+		} else if (valor > this.valorDisponivel) {
+			console.imprimir("O valor solicitado é superior ao disponível para saque.");
 		}
 		try {
 			return banco.sacar(sessao.getChave(), valor);
@@ -26,5 +36,5 @@ public class CaixaEletronico {
 			return false;
 		}
 	}
-	
+
 }
