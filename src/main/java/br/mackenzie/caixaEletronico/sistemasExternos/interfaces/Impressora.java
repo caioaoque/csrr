e247e1e1aa750c25fa2a21ce96cabe5892289be4
caioaoque <caioaoque@gmail.com>
2013-05-25 15:39:43 -0300

    Término sacar valor

--- src/main/java/br/mackenzie/caixaEletronico/sistemasExternos/interfaces/Impressora.java
+++ src/main/java/br/mackenzie/caixaEletronico/sistemasExternos/interfaces/Impressora.java
@@ -2,6 +2,6 @@ package br.mackenzie.caixaEletronico.sistemasExternos.interfaces;
 
 public interface Impressora {
 	
-	void imprimirBoleto(Object boleto); // TODO sera enviado um objeto boleto mesmo?	
+	void imprimirRecibo(String recibo);
 	
 }
